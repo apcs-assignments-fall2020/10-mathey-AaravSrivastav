a924b27528baeaa22d2503a847b53fdf22b53721
AaravSrivastav <22asrivastav@athenian.org>
2020-10-14 21:15:43 +0000

    added things

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,56 @@
 public class Mathey {
 
+
+    public static int max(int one, int two)
+    {
+        return Math.max(one, two);
+    }
+
+    public static int max(int one, int two, int three)
+    {
+        return max(one, max(two, three));
+    }
+
+    public static double max(double one, double two)
+    {
+        return Math.max(one, two);
+    }
+
+    public static double max(double one, double two, double three)
+    {
+        return max(one, max(two, three));
+    }
+
+
+
+    public static int min(int one, int two)
+    {
+      return Math.min(one, two);
+    }
+
+    public static int min(int one, int two, int three)
+    {
+        return min(one, min(two, three));
+    }
+
+    public static double min(double one, double two)
+    {
+        return Math.min(one, two);
+    }
+
+    public static double min(double one, double two, double three)
+    {
+        return min(one, min(two, three));
+    }
+
+
+    public static int randomInteger(int a, int b)
+    {
+        return (int)(Math.random() * (1+b-a) + a);
+    }
+
+    public static int randomInteger(int b)
+    {
+        return randomInteger(0, b);
+    }
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -7,7 +7,16 @@ public class MyMain {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int one = (int)(Math.random() * 11)+10;
+        int two = (int)(Math.random() * 11)+10;
+        int three = (int)(Math.random() * 11)+10;
+        System.out.println("the three random numbers are " + one + ", " + two + ", and " + three);
+        int smallest = 0;
+        int largest = 0;
+        smallest = Math.min(one, Math.min(two, three));
+        System.out.println("The smallest number is " + smallest);
+        largest = Math.max(one, Math.max(two, three));
+        System.out.println("The largest number is " + largest);
     }
 
 }
